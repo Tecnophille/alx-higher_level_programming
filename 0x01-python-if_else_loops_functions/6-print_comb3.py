0a99fbf9ef1cd33aa43aca5193d537c0e6e118a7
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-08-23 14:04:44 +0100

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for num in range(0, 90):
     if num % 10 > num / 10:
         if num != 89:
-        print("{:02d}, ".format(num), end='')
+    print("{:02d}, ".format(num), end='')
         else:
-        print("{:02d}".format(num))
+    print("{:02d}".format(num))
