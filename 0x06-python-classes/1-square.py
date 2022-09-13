d8f454c57d204d5c2fbc9ff700ecec7d1b33282d
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-09-13 21:44:01 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" this module defines a class Square"""
+
+
 class Square:
+    """ class for Square"""
     def __init__(self, size):
+        """ Initialize class """
         self.__size = size
