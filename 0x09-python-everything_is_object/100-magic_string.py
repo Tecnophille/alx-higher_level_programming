e459fa891e76b366a5b6d2bc55c2a8953eb115b9
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-09-20 22:02:49 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(l=[]):
-    l += ["BestSchool"]
-    return ", ".join(l)
+def magic_string(i=[0]):
+    i[0] += 1
+    return str("BestSchool, " * (i[0] - 1)) + "BestSchool"
