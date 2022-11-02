d8fc8d69e37177ca1a00e20232a1e803d2d968a6
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-11-02 11:51:46 +0100

    Update 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,5 @@
 -- Lists all shows in the database hbtn_0d_tvshows without a genre linked.
 -- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
-------------------------------------------------------------------------------
 SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
        LEFT JOIN `tv_show_genres` AS g
