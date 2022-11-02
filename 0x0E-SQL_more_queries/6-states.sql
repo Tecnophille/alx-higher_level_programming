e21e68fc4c4bd452c4ced401a862d0de11670f5f
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-11-02 07:41:58 +0100

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 -- Creates the table hbtn_0d_usa with table states.
+-----------------------------------------------------
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
     PRIMARY KEY(`id`),
