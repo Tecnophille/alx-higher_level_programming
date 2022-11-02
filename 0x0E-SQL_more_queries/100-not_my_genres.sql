7b20a7bc8b4a5ca4662ee866b6e29cbc9a258dc8
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-11-02 07:39:46 +0100

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,6 +1,7 @@
 -- Lists all genres of the database hbtn_0d_tvshows
 -- not linked to the show Dexter.
 -- Records are sorted by ascending genre name.
+-------------------------------------------------------
 SELECT DISTINCT `name`
   FROM `tv_genres` AS g
        INNER JOIN `tv_show_genres` AS s
