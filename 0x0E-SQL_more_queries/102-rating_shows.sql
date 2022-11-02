ad117740474fac9a5187d4742cfd3adcaf27668b
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-11-02 11:55:54 +0100

    TITLED (SOLVED)

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,5 @@
 -- Lists all shows from hbtn_0d_tvshows_rate by their rating.
 -- Records are ordered by descending rating.
-----------------------------------------------------
 SELECT `title`, SUM(`rate`) AS `rating`
   FROM `tv_shows` AS t
        INNER JOIN `tv_show_ratings` AS r
