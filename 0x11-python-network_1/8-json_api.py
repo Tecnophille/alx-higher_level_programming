ab01e095a97ead153f0a252f649f9e08ab565caf
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-12-16 12:39:35 +0100

    No result

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""
-Python script that sends a POST request to the URL and
-to an URL with the letter as a parameter
-"""
-import requests
-import sys
+"""Sends a request to the URL and displays the body of the response."""
 
 
-if __name__ == "__main__":
-    data = {'q': ""}
+if __name__ == '__main__':
+    from requests import post
+    from sys import argv
 
-    try:
-        data['q'] = sys.argv[1]
-    except:
-        pass
+    URL = 'http://0.0.0.0:5000/search_user'
+    data = {'q': argv[1] if len(argv) >= 2 else ""}
+    response = post(URL, data)
 
-    r = requests.post('http://0.0.0.0:5000/search_user', data)
+    type_res = response.headers['content-type']
 
-    try:
-        json_o = r.json()
-        if not json_o:
-            print("No result")
+    if type_res == 'application/json':
+        result = response.json()
+        _id = result.get('id')
+        name = result.get('name')
+        if (result != {} and _id and name):
+            print("[{}] {}".format(_id, name))
         else:
-            print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
-    except:
-        print("Not a valid JSON")
+            print('No result')
+    else:
+        print('Not a valid JSON')
