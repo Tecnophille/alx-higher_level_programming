07b601c431aaea7f6123b2c1bece1532c698f72d
Oliver Samuel <104834870+Tecnophille@users.noreply.github.com>
2022-12-16 12:33:53 +0100

    printing headers

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """
-Python script that sends a request to the URL and
-displays the value of a variable in the response header
-"""
-import requests
-import sys
+Sends a request to the URL and displays the value of the variable X-Request-Id
+in the response header"""
 
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
 
-if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
-        print(r.headers['X-Request-Id'])
-    except:
-        pass
+    res = get(argv[1])
+    print(res.headers.get('X-Request-Id'))
