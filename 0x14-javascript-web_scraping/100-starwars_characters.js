b9d31ca2a1fb809a9af80149d8ebfcc5bb897ee4
Oliver Samuel <oliversamuel146@gmail.com>
2023-01-23 00:31:53 +0100

    correx

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err == null) {
+    const resp = JSON.parse(body);
+    const characters = resp.characters;
+    for (let i = 0; i < characters.length; i++) {
+      request(characters[i], function (err, response, body) {
+        if (err == null) {
           console.log(JSON.parse(body).name);
         }
       });
-    });
+    }
   }
 });
