053156c41d4554eaceb6908f45b16438380cd254
Oliver Samuel <oliversamuel146@gmail.com>
2023-01-23 00:35:35 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,33 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
+const request = require('request');
+const urlMovie = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(urlMovie, async function (error, response, body) {
+  const arr = [];
+
+  if (error) {
+    console.log(error);
+  } else {
+    const film = JSON.parse(body);
+    for (let i = 0; i < film.characters.length; i++) {
+      arr.push(myCharacter(film.characters[i]));
     }
   }
+
+  let actors = await Promise.all(arr);
+
+  actors = actors.map((actor) => JSON.parse(actor).name);
+  actors.forEach((actor) => console.log(actor));
 });
+
+function myCharacter (thisCharacter) {
+  return new Promise((resolve, reject) => {
+    request(thisCharacter, function (error, response, body) {
+      if (error) {
+        reject(error);
+      }
+      resolve(body);
+    });
+  });
+}
